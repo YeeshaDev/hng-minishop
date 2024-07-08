79b92fa41472d0028e7e9ede0c76c53ea238690f
Aishat Agunbiade <101475738+YeeshaDev@users.noreply.github.com>
2024-07-08 21:47:48 +0100

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <main>
       {/* Hero */}
-      <div className="bg-heromobile sm:bg-hero w-full bg-black/20 bg-cover   bg-no-repeat h-[95vh] mt-5"></div>
+      <div className="bg-heromobile sm:bg-hero w-full bg-black/20 bg-cover   bg-no-repeat h-[80vh] sm:h-[95vh] mt-5"></div>
       <Categories />
       <Banner />
       <Products />
